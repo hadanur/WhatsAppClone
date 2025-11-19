b513d1cfcec459aeded4429c265e4b6b52ce112a
hadanur <115171695+hadanur@users.noreply.github.com>
2025-11-19 16:04:27 +0300

    start

---
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneApp.swift
+//  WhatsAppClone
+//
+//  Created by Hakan Adanur on 19/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
